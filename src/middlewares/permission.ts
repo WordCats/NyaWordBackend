ba070e1c24c841ba8bf04af0a81ff4922412ce70
HugoMarinn <hugomc13952@gmail.com>
2022-12-15 15:03:26 -0300

    Permission Middleware created successfully

--- src/middlewares/permission.ts
+++ src/middlewares/permission.ts
@@ -1,18 +1,87 @@
-// Not completed...
-
 import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { PrismaClient } from "@prisma/client";
 
-async function decodeToken(req: Request) {
-  const acessToken = req.headers.authorization;
+async function userRolesHavePermissionTo(permission: string, userId: number): Promise<boolean> {
+  const prisma = new PrismaClient();
+  await prisma.$connect();
+  
+  const rolesWithThisPermission = await prisma.permission.findUnique({
+    where: {
+      name: permission,
+    },
+    select: {
+      roles: {
+        select: {
+          role: {
+            select: {
+              name: true,
+            },
+          },
+        },
+      },
+    },
+  });
+
+  if (rolesWithThisPermission == null) {
+    throw new Error('Not exists this permission!');
+  }
+
+  const rolesNamesWithThisPermission = rolesWithThisPermission.roles.map(data => data.role.name);
+
+  const userRoles = await prisma.user_has_roles.findMany({
+    where: {
+      user_id: userId,
+    },
+    select: {
+      role: {
+        select: {
+          name: true,
+        }
+      },
+    },
+  });
+
+  if (userRoles == null) {
+    throw new Error('This user not have rules!');
+  }
+
+  const userRolesNames = userRoles.map(data => data.role.name);
+
+  await prisma.$disconnect();
+
+  let userPermitted = false;
+
+  userRolesNames.forEach(userRoleName => {
+    if (
+      rolesNamesWithThisPermission.includes(userRoleName)
+    ) userPermitted = true;
+  });
+
+  return userPermitted;
 }
 
-function userShouldCan(permissions: string[]) {
+export function userShouldCan(permissions: string[]) {
   const verifyUserPermissions = async (
-    req: Request, 
+    req: Request,
     res: Response, 
     next: NextFunction,
   ) => {
+    const acessToken = req.headers.authorization;
+    const { userId } = jwt.decode(acessToken!) as { userId: number };
     
+    for (const permission of permissions) {
+      const userHaveThisPermission = await userRolesHavePermissionTo(permission, userId);
+      
+      if (!userHaveThisPermission) {
+        return res.status(401).send({
+          status: 401,
+          message: `This user not have permission to: '${permission}'`,
+        });
+      }
+    }
+
+    next();
   }
 
   return verifyUserPermissions;
--- src/routes/history.ts
+++ src/routes/history.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
+import { userShouldCan } from "../middlewares/permission";
 import { TokenValidation } from "../middlewares/tokenValidation";
 import { createHistoryController } from "../useCases/History/CreateHistory";
 
 export const historyRouter = Router();
 
-historyRouter.post('/histories', TokenValidation, (req, res) => {
+historyRouter.use(TokenValidation);
+
+historyRouter.post('/histories', userShouldCan(['create:history']), (req, res) => {
   return createHistoryController.handle(req, res);
-})
+});
